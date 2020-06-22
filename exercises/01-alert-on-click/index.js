a0e7b97f2f3f14ae63a13165484cbc42b655abd6
Abel Segui <Abelsegui@hotmail.com>
2020-06-22 23:02:46 +0000

    02done

--- exercises/01-alert-on-click/index.js
+++ exercises/01-alert-on-click/index.js
@@ -1,3 +1,7 @@
 window.myClickFunction = function myClickFunction() {
 	alert("Your first function!");
 };
+let button = document.querySelector("#button2");
+button.addEventListener("click", function() {
+	alert("your Secound function");
+});
--- exercises/02-on-click-Hello-World/index.js
+++ exercises/02-on-click-Hello-World/index.js
@@ -1,1 +1,5 @@
 //Declare your function here
+let button = document.querySelector("#hello");
+button.addEventListener("mouseover", function() {
+	alert("Your first function!");
+});
